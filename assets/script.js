47f3ba80dedc417d70d241f64a79a5d13741d790
elizabeth berube <elizabethdberube@gmail.com>
2022-01-22 21:06:15 -0500

    Added loop. Fixed HTML. Needs debugging. Ugh

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,6 @@
 var timeDate = $('#time-date');
-var userInput = $('#user-input');
-var eventItem = $('#event-item');
-var saveButton = $('#button-addon2');
 var timeNow = moment().format("hh a");
+
 
 
 
@@ -11,84 +9,29 @@ function showTime() {
     timeDate.text(whatTime);
 
 }
+$('tbody tr').each(function () {
 
-$(document).ready(function () {
-    var $eventInput = $('#user-input');
-    if (userInput === "") {
-        return;
-    }
-    else (localStorage.getItem("#user-input"))
-    $eventInput.html(localStorage.getItem("#user-input"));
+    var tr = $(this);
+    var date_obj = moment(tr.find('th.hour').text());
+    var storage_key = tr.find('th.hour').text();
+    var user_input = tr.find("textarea.user-input");
+    var save_button = tr.find("button");
 
-    $('button').click(function () {
-        $eventInput.append($('#user-input').val());
-        localStorage.setItem("#user-input", $eventInput.html());
+    if (localStorage.getItem(storage_key))
+        user_input.val(localStorage.getItem(storage_key));
+
+    $(save_button).click(function () {
+        localStorage.setItem(storage_key, user_input.val());
+
+        if (date_obj < timeNow) {
+            $("th").addClass("past");
+
+        }
+
     });
 
 });
-//function displayMessage()
 
-//function handleInput(event) {
-//  var userInput = $('#user-input').val;
-//userInput = input;
-//event.preventDefault();
-//localStorage.setItem('input', 'input');
-//printInput()
-//}
-
-//function printInput() {
-//  var userInput = $('#user-input').val;
-//localStorage.getItem('input');
-//if (userInput === ""){
-//  displayMessage("error", "Input is blank");
-//}
-//else {
-//  displayMessage("success", "Your even is saved")
-//}
-//}
-//console.log(printInput());
-
-//$(function () {
-//    $("#draggable").draggable();
-//});
-
-//var printInput = function (data) {
-//  var inputLocat = $('<textarea>');
-//   var inputDetail = data;
-//   inputLocat.addClass('input-detail-item').text(inputDetail);
-//   inputLocat.appendTo(userInput);
-//}
-
-//var handleInput = function (event) {
-//event.preventDefault();
-
-//var userInputData = userInput.val();
-//
-//if (userInputData) {
-
-
-//}
-
-
-//if (userInputData = "") {
-
-//console.log('Form needs to be filled out');
-// {
-//      return;
-// }
-// };
-
-//}
-
-
-var timeManage = function () {
-
-}
-
-
-
-
-//saveButton.on('click', handleInput);
 
 
 
