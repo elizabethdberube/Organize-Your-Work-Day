21b00d0f1e30bd6d3376395f3518a2f349008503
elizabeth berube <elizabethdberube@gmail.com>
2022-01-23 19:53:34 -0500

    Added if statements to color code time slots Fixed issues with moment.js Need to delete uneeded stuff in HTML

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,46 @@
 var timeDate = $('#time-date');
-var timeNow = moment().format("hh a");
+var timeNow = moment().startOf('hour');
+//var whatHour = $('hour');
 
 
-
-
+//function for displaying date
 function showTime() {
     var whatTime = moment().format('dddd, MMMM Do, YYYY');
     timeDate.text(whatTime);
 
 }
+
+//function for saving events 
 $('tbody tr').each(function () {
 
     var tr = $(this);
-    var date_obj = moment(tr.find('th.hour').text());
+    var date_obj = moment(tr.find('th.hour').text(), 'h:mm A').startOf('hour');
     var storage_key = tr.find('th.hour').text();
     var user_input = tr.find("textarea.user-input");
     var save_button = tr.find("button");
 
+    if (date_obj.isSame(timeNow)) {
+        tr.addClass("present");
+
+    }
+
+    if (date_obj > timeNow) {
+        tr.addClass("future");
+
+    }
+
+    if (date_obj < timeNow) {
+        tr.addClass("past");
+
+    }
+
     if (localStorage.getItem(storage_key))
         user_input.val(localStorage.getItem(storage_key));
+    console.log(date_obj, timeNow);
 
     $(save_button).click(function () {
-        localStorage.setItem(storage_key, user_input.val());
+        localStorage.setItem(storage_key, user_input.val())
 
-        if (date_obj < timeNow) {
-            $("th").addClass("past");
-
-        }
 
     });
 
